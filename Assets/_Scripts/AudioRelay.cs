3f3c49cf7e6721672f27041565c556488a5e1d3e
miurasyoutaro <miurasyoutaro@gmail.com>
2025-09-17 10:42:17 +0900

    備忘録：AudioRelayのアタッチ忘れない

--- Assets/_Scripts/AudioRelay.cs
+++ Assets/_Scripts/AudioRelay.cs
@@ -12,4 +12,19 @@ public class AudioRelay : MonoBehaviour
         _inGameManager._SEAudioSource.clip = _inGameManager._SEAudioClipDict["N"].FirstOrDefault(c => c.name.Contains("0"));
         _inGameManager._SEAudioSource.Play();
     }
+    void NAttack001()
+    {
+        _inGameManager._SEAudioSource.clip = _inGameManager._SEAudioClipDict["N"].FirstOrDefault(c => c.name.Contains("1"));
+        _inGameManager._SEAudioSource.Play();
+    }
+    void NAttack002()
+    {
+        _inGameManager._SEAudioSource.clip = _inGameManager._SEAudioClipDict["N"].FirstOrDefault(c => c.name.Contains("2"));
+        _inGameManager._SEAudioSource.Play();
+    }
+    void NAttack003()
+    {
+        _inGameManager._SEAudioSource.clip = _inGameManager._SEAudioClipDict["N"].FirstOrDefault(c => c.name.Contains("3"));
+        _inGameManager._SEAudioSource.Play();
+    }
 }
